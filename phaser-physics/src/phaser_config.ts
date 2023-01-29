04a891109b38e1058ed97fc8194c763d647fada0
rajahwu <vmradford@gmail.com>
2023-01-28 20:01:36 -0500

    set up phaser physics game

---
+++ phaser-physics/src/phaser_config.ts
@@ -0,0 +1,14 @@
+import Phaser from 'phaser';
+
+export default {
+    type: Phaser.AUTO,
+    parent: 'game',
+    backgroundColor: '#000000',
+    scale: {
+        width: 400,
+        height: 500,
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    }
+
+} as Phaser.Types.Core.GameConfig
---
+++ phaser-physics/src/scenes/Game.ts
@@ -0,0 +1,7 @@
+import Phaser from "phaser";
+
+export default class Game extends Phaser.Scene {
+    constructor() {
+        super('game')
+    }
+}
