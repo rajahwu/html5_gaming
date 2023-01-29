0206fc90cd159c1bacda1f30211ccf46f7860e92
rajahwu <vmradford@gmail.com>
2023-01-28 19:35:52 -0500

    fix typescript errors

--- adventures_in_code/src/scenes/Level.ts
+++ adventures_in_code/src/scenes/Level.ts
@@ -5,10 +5,6 @@ export default class Level extends Phaser.Scene {
         super('level')
     }
     private circle?: Phaser.GameObjects.Arc
-    private player?: Phaser.Physics.Arcade.Sprite
-    private cursors?: Phaser.Types.Input.Keyboard.CursorKeys
-    // private ground?: Phaser.Physics.Arcade.StaticGroup
-    // private platform: Phaser.GameObjects.Rectangle
 
     preload() {
         this.load.image('codey', 'https://content.codecademy.com/courses/learn-phaser/codey.png')
@@ -16,10 +12,8 @@ export default class Level extends Phaser.Scene {
     }
 
     create() {
-        this.player = this.add.sprite(350, 200, 'codey')
+        this.add.sprite(350, 200, 'codey')
         this.circle = this.add.circle(25, 100, 20, 0xff0000)
-        // this.platform = this.add.rectangle(0, 500, 2500, 50, 0x00ff00)
-        // this.ground = this.physics.add.staticGroup()
 
 
     }
